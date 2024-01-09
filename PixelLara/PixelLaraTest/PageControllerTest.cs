db17c54604f43cdaa2a6c557cf238630875eaefd
Bence Nagy <nagybence0601@gmail.com>
2024-01-09 23:12:26 +0100

    Commit : Added test for the PageController actionmethod called GetAllPRoducts

--- PixelLara/PixelLaraTest/PageControllerTest.cs
+++ PixelLara/PixelLaraTest/PageControllerTest.cs
@@ -61,5 +61,48 @@ namespace PixelLaraTest
             Assert.AreEqual(expectedProducts, actualProducts);
 
         }
+        [Test]
+        public async Task GetProducts_ReturnsOkResultWithData()
+        {
+            // Arrange
+            var pageServiceMock = new Mock<IPageService>();
+            var controller = new ProductController(pageServiceMock.Object);
+
+            var expectedProducts = new List<ProductModel>
+            {
+                new ProductModel
+                {
+                    Id = 1,
+                    ShortDescription = "Sample Short Description 1",
+                    LongDescription = "Sample Long Description 1",
+                    DeadLine = DateTime.Now.AddDays(7),
+                    Contact = ContactEnum.Email,
+                    ContractTimeSet = DateTime.Now.AddDays(30)
+                },
+                new ProductModel
+                {
+                    Id = 2,
+                    ShortDescription = "Sample Short Description 2",
+                    LongDescription = "Sample Long Description 2",
+                    DeadLine = DateTime.Now.AddDays(14),
+                    Contact = ContactEnum.InPerson,
+                    ContractTimeSet = DateTime.Now.AddDays(15)
+                },
+            };
+
+            pageServiceMock.Setup(x => x.GetProductsAsync()).ReturnsAsync(expectedProducts);
+
+            // Act
+            var result = await controller.GetProducts();
+
+            // Assert
+            Assert.That(result.Result, Is.TypeOf<OkObjectResult>());
+            var okResult = (OkObjectResult)result.Result;
+            Assert.That(okResult.Value, Is.InstanceOf<List<ProductModel>>());
+            var actualProducts = (List<ProductModel>)okResult.Value;
+            Assert.AreEqual(expectedProducts, actualProducts);
+        }
     }
 }
+    
+
