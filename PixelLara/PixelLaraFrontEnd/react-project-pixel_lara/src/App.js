bb2af989839ca3e682118010a18788513c1dc37f
Bence Nagy <nagybence0601@gmail.com>
2024-01-21 20:26:49 +0100

    Added a frontend project, installed react

---
+++ PixelLara/PixelLaraFrontEnd/react-project-pixel_lara/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar>
+      </Navbar>
+    </Router>
+  );
+}
+
+export default App;
