1612ce34f023a7f3f932a8c744fedd093f5cb03a
Bence Nagy <nagybence0601@gmail.com>
2024-01-25 11:38:39 +0100

    Commit : Added a service link to navbar component

--- PixelLara/PixelLaraFrontEnd/react-project-pixel_lara/src/Navbar.js
+++ PixelLara/PixelLaraFrontEnd/react-project-pixel_lara/src/Navbar.js
@@ -8,6 +8,7 @@ const Navbar = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/service">Service</Link></li>
             </ul>
         </nav>
     )
