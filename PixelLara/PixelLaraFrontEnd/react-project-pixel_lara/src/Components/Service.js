206a06f1a07e04a9f32701d10255ff6c7e403e58
Bence Nagy <nagybence0601@gmail.com>
2024-02-02 15:02:42 +0100

    Commit : Deleted unnecessary files, created a plain about component

---
+++ PixelLara/PixelLaraFrontEnd/react-project-pixel_lara/src/Components/Service.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import AddProductForm from 'C:\Projects\PixelLara\PixelLara\PixelLaraFrontEnd\react-project-pixel_lara\src\Forms\ProductForms.js'
+
+
+function Service() {
+  return (
+        <AddProductForm/>
+  );
+}
+
+export default Service;
